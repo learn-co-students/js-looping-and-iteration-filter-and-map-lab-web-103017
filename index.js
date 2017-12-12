90167b3f6e582207e08ed1c8a5d6c762e0ce5a31
github username <github email address>
2017-12-11 21:43:33 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 // Code your solution here:
+function driversWithRevenueOver(driver, revenue) {
+  let arr = []
+  for (let x of driver) {
+    if (x.revenue > revenue) {
+      arr.push(x);
+    };
+  };
+  return arr
+};
+
+function driverNamesWithRevenueOver(driver, revenue) {
+  let arr = []
+  for (let x of driver) {
+    if (x.revenue > revenue) {
+      arr.push(x.name);
+    };
+  };
+  return arr
+};
+
+function exactMatch(driver, obj) {
+  let arr = [];
+  for (let x of driver) {
+    if (x.name == obj["name"]) {
+      arr.push(x)
+    } else if (x.revenue == obj["revenue"]) {
+      arr.push(x)
+    };
+  };
+  return arr
+};
+
+function exactMatchToList(driver, obj) {
+  let arr = [];
+  for (let x of driver) {
+    if (x.name == obj["name"]) {
+      arr.push(x.name)
+    } else if (x.revenue == obj["revenue"]) {
+      arr.push(x.name)
+    };
+  };
+  return arr
+};
